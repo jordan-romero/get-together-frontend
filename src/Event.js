9382082b182bec296ec5aae8697cf937665d8110
jordles113 <jordan.t.romero@gmail.com>
2020-09-25 11:21:36 -0500

    changes background for each card based on category

--- src/Event.js
+++ src/Event.js
@@ -17,14 +17,17 @@ class Event {
         const { name, description, duration, cost, location } = this.event
         const eventName = document.createElement('div')
         eventName.id = 'card-head'
-        eventName.className = 'card-header h2'
+        eventName.className = 'card-header h2 pr-5 mr-5'
         eventName.innerText = name
         const deleteBtn = document.createElement('button')
         deleteBtn.className = 'btn'
         deleteBtn.innerText = 'X'
         deleteBtn.id = 'event-delete-btn'
         this.deleteEvent(deleteBtn, card)
-        eventName.appendChild(deleteBtn)
+        const catName = document.createElement('h3')
+        catName.innerText = this.event.category.name
+        catName.id = 'cat-name-h3'
+        eventName.append(deleteBtn, catName)
         const eventDesc = document.createElement('p')
         eventDesc.className = 'card-body'
         eventDesc.id = 'desc-p'
@@ -37,6 +40,7 @@ class Event {
         eventDurationCost.id = 'dur-cost-p'
         eventDurationCost.className = 'card-footer'
         eventDurationCost.innerText = `${name} will cost ${cost} dollars and will be ${duration} long.`
+        this.renderCardBackground(card)
         card.append(eventName, eventDesc, eventLocation, eventDurationCost)
     }
 
@@ -48,6 +52,33 @@ class Event {
         app.appendChild(addBtn) 
 
         Event.eventListenerHandler(addBtn)
+    }
+
+    renderCardBackground(card) {
+        if (this.event.category.name === "Art & Culture") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80)"
+        } else if (this.event.category.name === "Comedy") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1527224857830-43a7acc85260?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80)"
+            card.style.backgroundSize = "47rem 20rem"
+        } else if (this.event.category.name === "Outdoor Activities") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1519331379826-f10be5486c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)"
+            card.style.backgroundSize = "50rem 23rem"
+        } else if (this.event.category.name === "Film & TV") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1568876694728-451bbf694b83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80)"
+            card.style.backgroundSize = "50rem 23rem"
+        } else if (this.event.category.name === "Food & Drink") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=931&q=80)"
+            card.style.backgroundSize = "50rem"
+        } else if (this.event.category.name === "Live Music") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)"
+            card.style.backgroundSize = "50rem 25rem"
+        } else if (this.event.category.name === "LGBTQ+") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1516119555254-e3b36f4b6769?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1062&q=80)"
+            card.style.backgroundSize = "50rem 25rem"
+        } else if (this.event.category.name === "Other") {
+            card.style.backgroundImage = "url(https://images.unsplash.com/photo-1543718290-a207a786243a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)"
+            card.style.backgroundSize = "50rem 25rem"
+        }
     }
 
     deleteEvent(deleteBtn, card) {
