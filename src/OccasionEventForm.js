093a8469853808f70c8dc2d5cd40f2f71fbe5db7
jordles113 <jordan.t.romero@gmail.com>
2020-10-01 17:03:09 -0500

    creates occasionEventForm and content

--- src/OccasionEventForm.js
+++ src/OccasionEventForm.js
@@ -1,12 +1,85 @@
 class OccasionEventForm {
 
     static createOccasionEventForm() {
-        alert('am wired up? ')
+        modal.style.display = "block"
+        const occEventForm = document.createElement('form')
+        occEventForm.id = 'event-form'
+        modalContent.append(occEventForm)
+        const categorySelector = OccasionEventForm.occEventFormContent(occEventForm)
+        occEventForm.categoryDropdown(categorySelector)
+        occEventForm.addEventListener('submit', occEventForm.handleFormSubmit)
     }
 
-    static postOccasionEvent(eventForm, e, card) {
+    static occEventFormContent(occEventForm){
+        const eventNameDiv = document.createElement('div')
+        eventNameDiv.className = 'form-group'
+        const eventNameLabel = document.createElement('label')
+        eventNameLabel.innerText = "Event Name:"
+        const eventNameInput = document.createElement('input')
+        eventNameInput.name = 'name'
+        eventNameInput.className = 'form-control'
+        eventNameDiv.append(eventNameLabel, eventNameInput)
+
+        const eventDescDiv = document.createElement('div')
+        eventDescDiv.className = 'form-group'
+        const eventDescLabel = document.createElement('label')
+        eventDescLabel.innerText = "Event Description:"
+        const eventDescInput = document.createElement('textarea')
+        eventDescInput.name = 'description'
+        eventDescInput.className = 'form-control'
+        eventDescDiv.append(eventDescLabel, eventDescInput)
+
+        const eventLocationDiv = document.createElement('div')
+        eventLocationDiv.className = 'form-group'
+        const eventLocationLabel = document.createElement('label')
+        eventLocationLabel.innerText = "Event Location:"
+        const eventLocationInput = document.createElement('textarea')
+        eventLocationInput.name = 'location'
+        eventLocationInput.className = 'form-control'
+        eventLocationDiv.append(eventLocationLabel, eventLocationInput)
+
+        const eventDurationDiv = document.createElement('div')
+        eventDurationDiv.className = 'form-group'
+        const eventDurationLabel = document.createElement('label')
+        eventDurationLabel.innerText = "Event Duration:"
+        const eventDurationInput = document.createElement('input')
+        eventDurationInput.name = 'duration'
+        eventDurationInput.className = 'form-control'
+        eventDurationDiv.append(eventDurationLabel, eventDurationInput)
+
+        const eventCostDiv = document.createElement('div')
+        eventCostDiv.className = 'form-group'
+        const eventCostLabel = document.createElement('label')
+        eventCostLabel.innerText = "Event Cost:"
+        const eventCostInput = document.createElement('input')
+        eventCostInput.name = 'cost'
+        eventCostInput.type = 'number'
+        eventCostInput.className = 'form-control'
+        eventCostDiv.append(eventCostLabel, eventCostInput)
+
+        const eventCatDiv = document.createElement('div')
+        eventCatDiv.className = 'form-group'
+        const categorySelectorLabel = document.createElement('label')
+        categorySelectorLabel.innerText = 'Category:'
+        const categorySelector = document.createElement('select')
+        categorySelector.id = 'select-category'
+        categorySelector.name = 'category'
+        categorySelector.className = 'form-control'
+        eventCatDiv.append(categorySelectorLabel, categorySelector)
+
+        const submitBtn = document.createElement('button')
+        submitBtn.className = 'btn'
+        submitBtn.id = 'event-submit'
+        submitBtn.innerText = "Submit"
+        occEventForm.append(eventNameDiv, eventDescDiv, eventLocationDiv, 
+            eventDurationDiv, eventCostDiv,
+            eventCatDiv, submitBtn)
+        return categorySelector
+    }
+
+    static postOccasionEvent(occEventForm, e, card) {
         
-        ApiService.postOccEvent(eventForm, occId)
+        ApiService.postOccEvent(occEventForm, occId)
             .then(response => {
                 console.log(response)
                 // this.occasion.events.push(response)
@@ -24,7 +97,7 @@ class OccasionEventForm {
     static handleFormSubmit(e) {
         e.preventDefault()
         modal.style.display = "none"
-        const eventForm = {
+        const occEventForm = {
           name: e.target["name"].value,
           description: e.target["description"].value,
           location: e.target["location"].value,
@@ -32,6 +105,6 @@ class OccasionEventForm {
           cost: e.target["cost"].value,
           category_name: e.target.category.value
         }
-        OccasionEventForm.postOccasionEvent(eventForm, e)
+        OccasionoccEventForm.postOccasionEvent(occEventForm, e)
       }
 }
